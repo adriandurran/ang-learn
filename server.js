4974a031d15247b85735b981964b2371964a621c
Adrian <adrian@auspiciousday.com>
2016-02-16 21:53:26 +0000

    troubles

--- server.js
+++ server.js
@@ -2,27 +2,45 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var Post = require('./server/models/post');
 var mongoose = require('mongoose');
+var morgan = require('morgan');
 
-mongoose.connect('mongodb://localhost/social', function() {
+mongoose.connect('mongodb://localhost/test', function(err) {
+	if (err) {throw err;}
 	console.log('mongodb connected');
 });
 
 var app = express();
 app.use(bodyParser.json());
 
-
-app.use(express.static(__dirname + '/client'));
-
+// app.use(express.static(__dirname + '/client'));
+app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
 	res.sendFile('client/layout/posts.html',  { root : __dirname});
 });
 
-app.get('/api/posts', function(req, res, next) {
+app.get('/users', function(req, res) {
 	Post.find(function(err, posts) {
-		if (err) {return next(err);}
+		if (err) {
+			console.log(err);
+			// return next(err);
+		}
 		res.json(posts);
 	});
+});
+
+app.get('/api/posts', function(req, res, next) {
+	res.json([
+		{username: "thebastard", body:"You fucking abstard"}
+		]);
+
+	// Post.find(function(err, posts) {
+	// 	if (err) {
+	// 		console.log(err);
+	// 		// return next(err);
+	// 	}
+	// 	res.json(posts);
+	// });
 });
 
 app.post('/api/posts', function(req, res) {
